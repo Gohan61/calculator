b4226a78ea9ec911738b0e93f0c63347d07ccc25
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2023-12-13 08:06:01 +0100

    Create operate function + operate & operand var
    Feat: added a operate function to be able to store user operator
    selection and operands inputs and perform correct calculation

--- JS/play.js
+++ JS/play.js
@@ -1,3 +1,7 @@
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = "";
+
 function add(...numbers) {
   console.log(numbers.reduce((a, b) => a + b));
 }
@@ -13,3 +17,22 @@ function multiply(...numbers) {
 function divide(...numbers) {
   console.log(numbers.reduce((a, b) => a / b));
 }
+
+function operate(operator, firstOperand, secondOperand) {
+  switch (operator) {
+    case "+":
+      add(firstOperand, secondOperand);
+      break;
+    case "-":
+      subtract(firstOperand, secondOperand);
+      break;
+    case "*":
+      multiply(firstOperand, secondOperand);
+      break;
+    case "/":
+      divide(firstOperand, secondOperand);
+      break;
+  }
+}
+
+operate("+", 1, 2);
