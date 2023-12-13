b7a9e0db52d67e98f60b1b1cd53f8051847e1de9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2023-12-13 10:19:03 +0100

    Add operator buttons + seperate containers
    Feat: added the operator buttons so appropriate functions can be
    selected for operations
    Feat: Added seperate divs for operator and operand buttons so they can
    be styled next to each other

--- JS/play.js
+++ JS/play.js
@@ -1,8 +1,19 @@
+//Variables
 const calculatorContainer = document.createElement("div");
+const operandsContainer = document.createElement("div");
+const operatorContainer = document.createElement("div");
 let firstOperand = 0;
 let secondOperand = 0;
 let operator = "";
+const addButton = document.createElement("button");
+const subtractButton = document.createElement("button");
+const multiplyButton = document.createElement("button");
+const divideButton = document.createElement("button");
+const equalsButton = document.createElement("button");
+const clearButton = document.createElement("button");
+const displayCalculation = document.createElement("div");
 
+//Operator functions
 function add(...numbers) {
   console.log(numbers.reduce((a, b) => a + b));
 }
@@ -19,6 +30,7 @@ function divide(...numbers) {
   console.log(numbers.reduce((a, b) => a / b));
 }
 
+//Calculate fucntion
 function operate(operator, firstOperand, secondOperand) {
   switch (operator) {
     case "+":
@@ -36,15 +48,49 @@ function operate(operator, firstOperand, secondOperand) {
   }
 }
 
-for (let i = 0; i < 10; i++) {
+//Buttons for operands + operators
+for (let i = 1; i < 10; i++) {
   const operandButtons = [];
   operandButtons.push(document.createElement("button"));
 
   operandButtons.forEach((button) => {
     button.setAttribute("class", "operands");
     button.textContent = `${i}`;
-    calculatorContainer.appendChild(button);
+    operandsContainer.appendChild(button);
   });
 }
 
+addButton.textContent = "+";
+subtractButton.textContent = "-";
+multiplyButton.textContent = "*";
+divideButton.textContent = "/";
+equalsButton.textContent = "=";
+clearButton.textContent = "Clear";
+
+[
+  addButton,
+  subtractButton,
+  multiplyButton,
+  divideButton,
+  equalsButton,
+  clearButton,
+].forEach((el) => el.setAttribute("class", "operators"));
+
+//Append to body
+calculatorContainer.setAttribute("class", "container");
 document.body.appendChild(calculatorContainer);
+calculatorContainer.append(operandsContainer, operatorContainer);
+operandsContainer.setAttribute("class", "operandsContainer");
+operatorContainer.setAttribute("class", "operatorContainer");
+
+operatorContainer.append(
+  addButton,
+  subtractButton,
+  multiplyButton,
+  divideButton,
+  equalsButton,
+  clearButton
+);
+
+displayCalculation.setAttribute("class", "display");
+calculatorContainer.appendChild(displayCalculation);
